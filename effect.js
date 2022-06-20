0cb73aa7b021c631cf3b675bb64f428a4f6f2f6d
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2022-06-20 17:53:58 -0400

    2

---
+++ effect.js
@@ -0,0 +1,21 @@
+// constantes de escenario
+const columnas = 40
+const filas = 30
+const lado = 20
+const ancho_canvas = columnas * lado
+const alto_canvas = filas * lado
+
+// variables de escenario
+let serpiente
+let comida
+
+// variables de control
+let arriba
+let derecha
+let izquierda
+let abajo
+
+// variables de entorno html
+let canvas
+
+
